8e6b5c019d125c7edb33cdb2d53157ebff3657f7
feli-web <felipefunes2002@gmail.com>
2022-11-18 00:42:57 -0300

    Initial commit

---
+++ Assets/Scripts/moneda2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moneda2 : MonoBehaviour
+{
+    int coins = 0;
+
+    [SerializeField] Text coinsText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coins++;
+            coinsText.text = "SCORE: " + coins;
+        }
+
+  }
+
+}
---
+++ Assets/Scripts/pressR.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pressR : MonoBehaviour
+
+{
+   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("level1");
+        }
+    }
+
+
+}
+// FindObjectOfType<manager>().EndGame();
